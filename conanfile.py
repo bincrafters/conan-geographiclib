f85d4489402098de6c538cee3923db4652d7d2da
danimtb <danimanzaneque@gmail.com>
2018-10-12 14:35:36 +0200

    Update to new recipe conventions and use new docker images

--- conanfile.py
+++ conanfile.py
@@ -19,9 +19,9 @@ class GeographiclibConan(ConanFile):
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
+    default_options = {"shared": False, "fPIC": True}
+    _source_subfolder = "_source_subfolder"
+    _build_subfolder = "_build_subfolder"
 
     def config_options(self):
         if self.settings.os == 'Windows':
@@ -34,23 +34,23 @@ class GeographiclibConan(ConanFile):
         tools.download("{0}/{1}-{2}.tar.gz".format(source_url, name, self.version), filename=filename)
         tools.unzip(filename)
         extracted_dir = name + '-' + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
-    def configure_cmake(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions['GEOGRAPHICLIB_LIB_TYPE'] = 'SHARED' if self.options.shared else 'STATIC'
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
         # it does not work on Windows but is not needed
-        tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeLists.txt"), "add_subdirectory (js)", "")
-        cmake = self.configure_cmake()
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "add_subdirectory (js)", "")
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
         cmake.install()
         # there is no option to disable subdirectories
         for folder in ["share", os.path.join("lib", "python"), "sbin", "python", "matlab", "doc"]:
